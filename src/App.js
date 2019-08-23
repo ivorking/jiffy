b1d602e6aac26d3148f4c0e27c43d5705c43ecd4
Ivor King <ivorking@gmail.com>
2019-08-23 09:13:06 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { timingSafeEqual } from "crypto";
 
 const Header = () => (
    <div className = 'header grid'>
@@ -7,13 +8,50 @@ const Header = () => (
 )
 
 class App extends Component {
+
+   constructor(props) {
+      super(props);
+      this.state = {
+         searchTerm: ''
+      }
+   }
+
+   handleChange = event => {
+      const {value} = event.target;
+      this.setState((prevState, props) => ({
+         // take old props
+         ...prevState,
+         // overwrite old props with new values
+         searchTerm: value
+      }));
+      console.log(value)
+      if (value.length > 2 ) {
+
+      }
+   };
+
+   handleKeyPress = event => {
+      console.log(event.key)
+      const {value} = event.target;
+
+      if (value.length > 2 && event.key === 'Enter') {
+         alert(`search for ${value}`);
+      }
+   }
+
    render() {
+      const {searchTerm} = this.state
       return (
          <div className="page">
             <Header />
             <div className="search grid">
-               <input className="input grid-item" placeholder="Type something" />
-
+               <input 
+                  className="input grid-item" 
+                  placeholder="Type something" 
+                  onChange={this.handleChange}
+                  onKeyPress={this.handleKeyPress}
+                  value={searchTerm}
+               />
             </div>
          </div>
       );
